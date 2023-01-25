f49ad02f422c1131476da0189cb2264f39f15fa3
Sergio <98350551+sergiofgdev@users.noreply.github>
2023-01-25 19:35:04 +0100

    Project created

---
+++ code.js
@@ -0,0 +1,48 @@
+let screen = document.querySelector(".screen")
+const btnNumber = document.querySelectorAll(".number")
+
+let data = ""
+console.log(typeof data)
+
+btnNumber.forEach(element => {
+    element.addEventListener('click', () => {
+        console.log(element.id)
+        data = parseInt(data + element.id)
+        screen.textContent = data
+        // console.log(typeof data)
+
+    })
+});
+
+//operate
+const operate = function (operator, a, b) {
+    operator(a, b)
+
+}
+
+//add
+const add = function (a, b) {
+    console.log("add")
+
+}
+
+//subtract
+const subtract = function (a, b) {
+    console.log("substract")
+
+
+}
+
+//multiply
+const multiply = function (a, b) {
+    console.log("multiply")
+
+
+}
+
+//divide
+const divide = function (a, b) {
+    console.log("divide")
+
+
+}
